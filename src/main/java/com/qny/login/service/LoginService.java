9043a927ba3e50c979bed17efc05228362f3214c
汉槿 <hanjin@jushitan.com>
2021-05-29 11:59:36 +0800

    代码初始化

---
+++ src/main/java/com/qny/login/service/LoginService.java
@@ -0,0 +1,11 @@
+package com.qny.login.service;
+
+/**
+ * 用户登录业务层
+ *
+ * @author 汉槿
+ * @version 1.0
+ * @since 2021-05-29 11:54
+ */
+public interface LoginService {
+}
---
+++ src/main/java/com/qny/login/service/LoginServiceImpl.java
@@ -0,0 +1,16 @@
+package com.qny.login.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * 用户登录业务层实现
+ *
+ * @author 汉槿
+ * @version 1.0
+ * @since 2021-05-29 11:55
+ */
+@Service
+@Slf4j
+public class LoginServiceImpl implements LoginService{
+}
